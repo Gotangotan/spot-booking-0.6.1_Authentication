69d709e7d166b716be48e0807dd7667e19f12d5c
Tan Nguyen <tannguyen85@gmail.com>
2021-04-23 20:53:06 +0200

    ClientsController CRUD

--- src/main/java/nl/spotbooking/spotbooking/controller/ClientsController.java
+++ src/main/java/nl/spotbooking/spotbooking/controller/ClientsController.java
@@ -2,20 +2,49 @@ package nl.spotbooking.spotbooking.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 public class ClientsController {
-    @GetMapping(value=  "/clients")
-    public ResponseEntity<Object> getClients() {
-        List<String> data = new ArrayList<>();
-        data.add("Jan");
-        data.add("Piet");
-        data.add("Joris");
-        return new ResponseEntity<Object>(data, HttpStatus.OK);
+
+    Map<Long, String> data = new HashMap<>();
+
+    // constructor
+    ClientsController() {
+        this.data.put(1L, "Jansen");
+        this.data.put(2L, "Pietersen");
+        this.data.put(3L, "Kardol");
     }
+
+    @GetMapping(value = "/clients")
+    public ResponseEntity<Object> getClients() {
+        return new ResponseEntity<Object>(this.data.values(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/clients/{id}")
+    public ResponseEntity<Object> getClient(@PathVariable("id") Long id) {
+        return new ResponseEntity<Object>(this.data.get(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/clients/{id}")
+    public ResponseEntity<Object> deleteClient(@PathVariable("id") Long id) {
+        this.data.remove(id);
+        return new ResponseEntity<Object>("Record deleted", HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/clients")
+    public ResponseEntity<Object> addClient(@RequestBody String clientName) {
+        long maxID = this.data.keySet().stream().max(Comparator.comparing(Long::valueOf)).get();
+        this.data.put(maxID + 1, clientName);
+        return new ResponseEntity<Object>("Record created", HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/clients/{id}")
+    public ResponseEntity<Object> updateClient(@PathVariable("id") Long id, @RequestBody String clientName) {
+        this.data.put(id, clientName);
+        return new ResponseEntity<Object>("Record updated", HttpStatus.OK);
+    }
+
 }
