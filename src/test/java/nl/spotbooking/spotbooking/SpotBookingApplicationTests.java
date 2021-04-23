e73873c31d7afdc51337e5fb7d536131333ae47a
Tan Nguyen <tannguyen85@gmail.com>
2021-04-23 15:56:07 +0200

    Create project, test with hello world

---
+++ src/test/java/nl/spotbooking/spotbooking/SpotBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.spotbooking.spotbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
