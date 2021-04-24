238109be925bc1caf106b42fd9d7dc58fac424aa
Tan Nguyen <tannguyen85@gmail.com>
2021-04-24 16:23:44 +0200

    create Seats class

---
+++ src/main/java/nl/spotbooking/spotbooking/model/Seats.java
@@ -0,0 +1,41 @@
+package nl.spotbooking.spotbooking.model;
+
+//POJO - Plain Old Java Object
+
+import javax.persistence.*;
+
+@Entity
+public class Seats {
+
+    // attributen die overeenkomen met de columns in PostgreSQL/SpotBooking/Table/Columns
+
+    //PrimaryKey
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String Desk;
+
+    @Column
+    private String Availability;
+
+
+    //getters and setters
+
+    public String getDesk() {
+        return Desk;
+    }
+
+    public void setDesk(String desk) {
+        Desk = desk;
+    }
+
+    public String getAvailability() {
+        return Availability;
+    }
+
+    public void setAvailability(String availability) {
+        Availability = availability;
+    }
+}
