1c3352023082bf0230ded03447052d303a8cc36a
Tan Nguyen <tannguyen85@gmail.com>
2021-04-25 09:01:57 +0200

    created seats by ID GetMapping

--- src/main/java/nl/spotbooking/spotbooking/controller/SeatController.java
+++ src/main/java/nl/spotbooking/spotbooking/controller/SeatController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class SeatController {
@@ -20,6 +22,11 @@ public class SeatController {
     public ResponseEntity<Object> getSeats() {
         List<Seat> seats = seatRepository.findAll();
         return new ResponseEntity<>(seats, HttpStatus.OK);
+    }
+
+    @GetMapping(value="/seats/{id}")
+    public ResponseEntity<Optional<Seat>> getSeat(@PathVariable("id") long id){
+        return new ResponseEntity<>(seatRepository.findById(id),HttpStatus.OK);
     }
 
 }
