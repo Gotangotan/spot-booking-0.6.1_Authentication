88c047f6e1e02df04c9bc5b7e8d23169e5dda855
Tan Nguyen <tannguyen85@gmail.com>
2021-04-25 09:48:30 +0200

    created seats by ID GetMapping

--- src/main/java/nl/spotbooking/spotbooking/controller/SeatController.java
+++ src/main/java/nl/spotbooking/spotbooking/controller/SeatController.java
@@ -5,9 +5,7 @@ import nl.spotbooking.spotbooking.repository.SeatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,6 +25,18 @@ public class SeatController {
     @GetMapping(value="/seats/{id}")
     public ResponseEntity<Optional<Seat>> getSeat(@PathVariable("id") long id){
         return new ResponseEntity<>(seatRepository.findById(id),HttpStatus.OK);
+    }
+
+    @PostMapping(value="/seats")
+    public ResponseEntity<Object> createSeat(@RequestBody Seat seat){
+        seatRepository.save(seat);
+        return new ResponseEntity<>("Seat Created",HttpStatus.CREATED);
+    }
+
+    @DeleteMapping (value="/seats/{id}")
+    public ResponseEntity<Object> deleteSeat(@PathVariable("id") long id){
+        seatRepository.deleteById(id);
+        return new ResponseEntity<>("Seat deleted", HttpStatus.OK);
     }
 
 }
