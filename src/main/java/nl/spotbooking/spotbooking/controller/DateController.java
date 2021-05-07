171c4172d00140a3d44839dfa1d1d1641e3956b7
Tan Nguyen <tannguyen85@gmail.com>
2021-05-07 10:56:23 +0200

    implement  many to one/ one to many

--- src/main/java/nl/spotbooking/spotbooking/controller/DateController.java
+++ src/main/java/nl/spotbooking/spotbooking/controller/DateController.java
@@ -15,23 +15,25 @@ public class DateController {
     @Autowired
     private DateService dateService;
 
+    @CrossOrigin
     @GetMapping(value="/date")
     public ResponseEntity<Object> getDates() {
         List<Date> dates = dateService.getAllDates();
         return new ResponseEntity<>(dates, HttpStatus.OK);
     }
 
+    @CrossOrigin
     @GetMapping(value="/date/{id}")
     public ResponseEntity<Object> getDate(@PathVariable("id") long id){
         return new ResponseEntity<>(dateService.getDate(id),HttpStatus.OK);
     }
-
+    @CrossOrigin
     @PostMapping(value="/date")
     public ResponseEntity<Object> createDate(@RequestBody Date date){
         dateService.save(date);
         return new ResponseEntity<>("Date Created",HttpStatus.CREATED);
     }
-
+    @CrossOrigin
     @DeleteMapping (value="/date/{id}")
     public ResponseEntity<Object> deleteDate(@PathVariable("id") long id){
         dateService.deleteById(id);
--- src/main/java/nl/spotbooking/spotbooking/controller/DeskController.java
+++ src/main/java/nl/spotbooking/spotbooking/controller/DeskController.java
@@ -17,29 +17,30 @@ public class DeskController {
     @Autowired
     private DeskService deskService;
 
+    @CrossOrigin
     @GetMapping(value="/desk")
     public ResponseEntity<Object> getDesks() {
         List<Desk> desks = deskService.getAllDesk();
         return new ResponseEntity<>(desks, HttpStatus.OK);
     }
-
+    @CrossOrigin
     @GetMapping(value="/desk/{id}")
     public ResponseEntity<Object> getDesk(@PathVariable("id") long id){
         return new ResponseEntity<>(deskService.getDesk(id),HttpStatus.OK);
     }
-
+    @CrossOrigin
     @PostMapping(value="/desk")
     public ResponseEntity<Object> createDesk(@RequestBody Desk desk){
         deskService.save(desk);
         return new ResponseEntity<>("Desk Created",HttpStatus.CREATED);
     }
-
+    @CrossOrigin
     @DeleteMapping (value="/desk/{id}")
     public ResponseEntity<Object> deleteDesk(@PathVariable("id") long id){
         deskService.deleteById(id);
         return new ResponseEntity<>("Desk deleted", HttpStatus.OK);
     }
-
+    @CrossOrigin
     @PutMapping(value = "/desk/{id}")
     public ResponseEntity<Object> updateBook(@PathVariable("id") long id, @RequestBody Desk desk) {
         deskService.updateDesk(id, desk);
