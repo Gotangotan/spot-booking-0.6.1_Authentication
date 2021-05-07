1a5eac53e1134b5a4a5a8ceac092eebb498cc737
Tan Nguyen <tannguyen85@gmail.com>
2021-05-07 14:52:13 +0200

    Add SpringSecurityConfig settings

--- src/main/java/nl/spotbooking/spotbooking/controller/config/SpringSecurityConfig.java
+++ src/main/java/nl/spotbooking/spotbooking/controller/config/SpringSecurityConfig.java
@@ -1,5 +1,40 @@
 package nl.spotbooking.spotbooking.controller.config;
 
-public class SpringSecurityConfig {
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+
+    }
+
+    // Secure the endpoints with HTTP Basic authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
 }
