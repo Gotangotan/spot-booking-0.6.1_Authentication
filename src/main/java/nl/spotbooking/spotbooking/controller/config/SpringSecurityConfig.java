e620a2b8632ffacb959cbdbfc529d8a83dc7f020
Tan Nguyen <tannguyen85@gmail.com>
2021-05-08 17:39:48 +0200

    added User and Authority to model

--- src/main/java/nl/spotbooking/spotbooking/controller/config/SpringSecurityConfig.java
+++ src/main/java/nl/spotbooking/spotbooking/controller/config/SpringSecurityConfig.java
@@ -1,28 +1,38 @@
 package nl.spotbooking.spotbooking.controller.config;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Autowired
+    private DataSource dataSource;
 
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource);
 
     }
 
-    // Secure the endpoints with HTTP Basic authentication
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Secure the endpoins with HTTP Basic authentication
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
@@ -31,11 +41,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/desk/**").hasRole("USER")
+                .antMatchers(HttpMethod.GET, "/admin/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/authenticated/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .csrf().disable()
                 .formLogin().disable();
-
     }
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -23,3 +23,14 @@ insert into desk (desk, availability,date_id) values ('Desk7','Available',2);
 insert into desk (desk, availability,date_id) values ('Desk8','Available',2);
 insert into desk (desk, availability,date_id) values ('Desk9','Available',2);
 insert into desk (desk, availability,date_id) values ('Desk10','Available',2);
+
+
+INSERT INTO users (username, password, enabled) VALUES ('user', '$2a$10$wPHxwfsfTnOJAdgYcerBt.utdAvC24B/DWfuXfzKBSDHO0etB1ica', TRUE);
+INSERT INTO users (username, password, enabled) VALUES ('admin', '$2a$10$wPHxwfsfTnOJAdgYcerBt.utdAvC24B/DWfuXfzKBSDHO0etB1ica', TRUE);
+INSERT INTO users (username, password, enabled) VALUES ('peter', '$2a$10$wPHxwfsfTnOJAdgYcerBt.utdAvC24B/DWfuXfzKBSDHO0etB1ica', TRUE);
+
+INSERT INTO authorities (username, authority) VALUES ('user', 'ROLE_USER');
+INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_USER');
+INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_ADMIN');
+INSERT INTO authorities (username, authority) VALUES ('peter', 'ROLE_USER');
+INSERT INTO authorities (username, authority) VALUES ('peter', 'ROLE_ADMIN');
