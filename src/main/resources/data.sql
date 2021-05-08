4b2d6e0e732e780335f19ec4561db085ac96e95c
Tan Nguyen <tannguyen85@gmail.com>
2021-05-08 18:27:22 +0200

    changes to data.sql injection

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -27,10 +27,10 @@ insert into desk (desk, availability,date_id) values ('Desk10','Available',2);
 
 INSERT INTO users (username, password, enabled) VALUES ('user', '$2a$10$wPHxwfsfTnOJAdgYcerBt.utdAvC24B/DWfuXfzKBSDHO0etB1ica', TRUE);
 INSERT INTO users (username, password, enabled) VALUES ('admin', '$2a$10$wPHxwfsfTnOJAdgYcerBt.utdAvC24B/DWfuXfzKBSDHO0etB1ica', TRUE);
-INSERT INTO users (username, password, enabled) VALUES ('peter', '$2a$10$wPHxwfsfTnOJAdgYcerBt.utdAvC24B/DWfuXfzKBSDHO0etB1ica', TRUE);
+INSERT INTO users (username, password, enabled) VALUES ('tan', '$2a$10$wPHxwfsfTnOJAdgYcerBt.utdAvC24B/DWfuXfzKBSDHO0etB1ica', TRUE);
 
 INSERT INTO authorities (username, authority) VALUES ('user', 'ROLE_USER');
 INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_USER');
 INSERT INTO authorities (username, authority) VALUES ('admin', 'ROLE_ADMIN');
-INSERT INTO authorities (username, authority) VALUES ('peter', 'ROLE_USER');
-INSERT INTO authorities (username, authority) VALUES ('peter', 'ROLE_ADMIN');
+INSERT INTO authorities (username, authority) VALUES ('tan', 'ROLE_USER');
+INSERT INTO authorities (username, authority) VALUES ('tan', 'ROLE_ADMIN');
